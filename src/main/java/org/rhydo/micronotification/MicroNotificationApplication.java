aa6f41bc50d4c2b738fec1ff35df59a5fa5e6b04
zerthyn <zerthyn7@gmail.com>
2025-08-13 07:04:23 +0800

    First commit

---
+++ src/main/java/org/rhydo/micronotification/MicroNotificationApplication.java
@@ -0,0 +1,13 @@
+package org.rhydo.micronotification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroNotificationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroNotificationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rhydo/micronotification/MicroNotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rhydo.micronotification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroNotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
